93bee69506373ec721ff2d80964ed4603480f6c9
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-26 20:41:23 +0600

    follow done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
   const user = await currentUser();
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      <div className="lg:col-span-7">
+      <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
       </div>
       
-      <div className="hidden lg:block lg:col-span-3 sticky top-20">
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
         <WhoToFollow />
       </div>
     </div>
