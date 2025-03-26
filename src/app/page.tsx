f82d43b65dcd500633f46b2a9d91f65bdf738e28
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-26 09:19:50 +0600

    desktop navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
 
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant={"secondary"}>click me</Button>
+      <h1>Home page content</h1>
     </div>
   );
 }
