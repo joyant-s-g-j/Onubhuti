dad1ef08992c96aa25debd3e66c894806fcda46b
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-26 18:37:04 +0600

    adding textarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>Home page content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-7">
+        {user ? <CreatePost /> : null}
+      </div>
+
+      <div className="hidden lg:block lg:col-span-3 sticky top-20">
+        WhoToFollow
+      </div>
     </div>
   );
 }
