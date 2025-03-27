ad84ebfa98a1cc8b51c41d85abbee2cc0759253d
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-27 10:25:05 +0600

    comment create function done

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -132,3 +132,50 @@ export async function toggleLike(postId: string) {
         return {success: false, error: "Failed to toggle like"}
     }
 }
+
+export async function createComment(postId: string, content: string) {
+    try {
+        const userId = await getDbUserId();
+        if(!userId) return;
+        if(!content) throw new Error("Content is required")
+        
+        const post = await prisma.post.findUnique ({
+            where: {
+                id: postId,
+            },
+            select: {
+                authorId: true
+            }
+        })
+
+        if (!post) throw new Error("Post not found")
+
+        const [comment] = await prisma.$transaction(async (tx) => {
+            const newComment = await tx.comment.create({
+                data: {
+                    content,
+                    authorId: userId,
+                    postId,
+                },
+            });
+
+            if(post.authorId !== userId) {
+                await tx.notification.create ({
+                    data: {
+                        type: "COMMENT",
+                        userId: post.authorId,
+                        creatorId: userId,
+                        postId,
+                        commentId: newComment.id
+                    }
+                })
+            }
+            return [newComment]
+        });
+        revalidatePath(`/`)
+        return {success: true, comment}
+    } catch (error) {
+        console.error("Failed to create comment:", error)
+        return {success: false, error: "Failed to create comment"}
+    }
+}
